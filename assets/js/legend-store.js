8420743af0c0982684c09b5e530a243c6581c178
Justin <j.hebenstreit25@gmail.com>
2024-07-27 21:58:41 -0500

    added more bootstrap and some JS

--- assets/js/legend-store.js
+++ assets/js/legend-store.js
@@ -14,39 +14,39 @@
 //
 
 
-const legendStore = [{
-    manufacturer: "BMW",
-    model: "Z4 LCI E89",
-    bp_1_price: 10000,
-    bp_2_price: 15000,
-    bp_3_price: 20000,
-    bp_4_price: 25000,
-    bp_5_price: 30000,
-    total: this.bp_1_price + this.bp_2_price + this.bp_3_price + this.bp_4_price + this.bp_5_price
-}, {
-    manufacturer: "Chevrolet",
-    model: "Camaro LT",
-    bp_1_price: 10000,
-    bp_2_price: 15000,
-    bp_3_price: 20000,
-    bp_4_price: 25000,
-    bp_5_price: 30000,
-    total: this.bp_1_price + this.bp_2_price + this.bp_3_price + this.bp_4_price + this.bp_5_price
-}, {
-    manufacturer: "Lotus",
-    model: "Elise Sprint 220",
-    bp_1_price: 33000,
-    bp_2_price: 50000,
-    bp_3_price: 67000,
-    bp_4_price: 84000,
-    bp_5_price: 101000,
-    total: this.bp_1_price + this.bp_2_price + this.bp_3_price + this.bp_4_price + this.bp_5_price
-}]
+// const legendStore = [{
+//     manufacturer: "BMW",
+//     model: "Z4 LCI E89",
+//     bp_1_price: 10000,
+//     bp_2_price: 15000,
+//     bp_3_price: 20000,
+//     bp_4_price: 25000,
+//     bp_5_price: 30000,
+//     total: this.bp_1_price + this.bp_2_price + this.bp_3_price + this.bp_4_price + this.bp_5_price
+// }, {
+//     manufacturer: "Chevrolet",
+//     model: "Camaro LT",
+//     bp_1_price: 10000,
+//     bp_2_price: 15000,
+//     bp_3_price: 20000,
+//     bp_4_price: 25000,
+//     bp_5_price: 30000,
+//     total: this.bp_1_price + this.bp_2_price + this.bp_3_price + this.bp_4_price + this.bp_5_price
+// }, {
+//     manufacturer: "Lotus",
+//     model: "Elise Sprint 220",
+//     bp_1_price: 33000,
+//     bp_2_price: 50000,
+//     bp_3_price: 67000,
+//     bp_4_price: 84000,
+//     bp_5_price: 101000,
+//     total: this.bp_1_price + this.bp_2_price + this.bp_3_price + this.bp_4_price + this.bp_5_price
+// }]
 
-function searchManufacturer(car) {
-    return car.manufacturer === "BMW"
-}
+// function searchManufacturer(car) {
+//     return car.manufacturer === "BMW"
+// }
 
-const carSearch = legendStore.find(searchManufacturer)
+// const carSearch = legendStore.find(searchManufacturer)
 
-console.log(carSearch)
+// console.log(carSearch)
---
+++ assets/js/unite-home.js
@@ -0,0 +1,6 @@
+const homeImageCarousel = document.querySelector('asphalt-carousel-slides')
+
+const asphaltCarousel = new bootstrap.Carousel(homeImageCarousel, {
+    interval: 1200,
+
+})
